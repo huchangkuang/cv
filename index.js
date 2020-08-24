7bc75a2322b234e49178dce7ef0ad4c294159f4b
huchangkuang <603021137@qq.com>
2020-08-24 15:17:03 +0800

    完成记账链接及二维码

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const previewVue = document.querySelector("#preview-vue")
-const qrcode = document.querySelector(".account_vue")
+const previewReact = document.querySelector("#preview-react")
+const qrcode_vue = document.querySelector(".account_vue")
+const qrcode_react = document.querySelector(".account_react")
 const width = document.documentElement.clientWidth
 function previewQrCode(target, show) {
     target.addEventListener("mouseenter", () => {
@@ -12,5 +14,6 @@ function previewQrCode(target, show) {
     })
 }
 if (width > 500) {
-    previewQrCode(previewVue, qrcode)
+    previewQrCode(previewVue, qrcode_vue)
+    previewQrCode(previewReact, qrcode_react)
 }
